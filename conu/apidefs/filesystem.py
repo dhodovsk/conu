64aa873d86fc763e870f1d4087d4e749e3c41090
Dominika Hodovska <dhodovsk@redhat.com>
2017-12-07 13:52:51 +0100

    Doc pre generation fix

--- conu/apidefs/filesystem.py
+++ conu/apidefs/filesystem.py
@@ -17,7 +17,6 @@
 import logging
 import os
 import shutil
-import xattr
 from tempfile import mkdtemp
 
 from conu.exceptions import ConuException
@@ -161,4 +160,3 @@ class Filesystem(object):
         """
         # what if SELinux is not enabled?
         p = self.p(file_path)
-        return xattr.get(p, "security.selinux")
